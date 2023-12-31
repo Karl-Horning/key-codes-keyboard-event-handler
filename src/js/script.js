ef35e7084875439a341c7b3fcf81ecf0c9a19b3e
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2023-12-31 10:33:54 +0000

    Added JSDoc, comments, and minor improvements

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,20 @@
-function checkKeycode(e) {
+/**
+ * Function to handle key events and display key code.
+ * @param {KeyboardEvent} e - The event object representing a keyboard event.
+ */
+const checkKeycode = (e) => {
+    // Select the element with the class "keycode" to display the key code
     const cKeyCode = document.querySelector(".keycode");
-    let key = e.key;
-    const keycode = e.keyCode;
 
-    if (key === " ") {
-        key = "Space";
-    }
+    // Use the 'key' property for modern browsers and 'keyCode' as a fallback
+    let key = e.key || String.fromCharCode(e.keyCode);
 
-    cKeyCode.textContent = `${key}: ${keycode}`;
-}
+    // Replace space key representation
+    key = key === " " ? "Space" : key;
 
-document.addEventListener("keyup", checkKeycode);
+    // Display the key and key code in the selected element
+    cKeyCode.textContent = `${key}: ${e.keyCode}`;
+};
+
+// Use the 'keydown' event for better handling of non-character keys
+document.addEventListener("keydown", checkKeycode);
